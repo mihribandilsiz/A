3ad9295ecfb4edc74d90369bb7d23361d6153e41
mihribandilsiz <108831031+mihribandilsiz@users.noreply.github.com>
2022-09-21 13:28:46 +0300

    Add files via upload

---
+++ javademos/datatypes/src/datatypes/Main.java
@@ -0,0 +1,24 @@
+package datatypes;
+
+public class Main {
+
+	public static void main(String[] args) {
+		//tam sayı
+		int sayi = 12;
+		
+		//ondalıklı sayı
+	    double sayi1 = 12.5;
+	    sayi = -129;
+	    
+	    // tek tırnak içinde yazılır.Karakter tutar.
+	    char karakter ='A';
+	    		
+	    		//dogru veya yanlış. 0 veya 1 demektir
+	    		boolean dogruMu = false ;
+	 
+	    
+	    
+	    	 
+	}
+
+}
